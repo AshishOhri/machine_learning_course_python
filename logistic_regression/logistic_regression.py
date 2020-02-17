5198242a9d109732e9c293c7031c476b7df523d0
Ashish <ashishohri99@gmail.com>
2020-02-17 18:13:07 +0530

    logistic regression

--- logistic_regression/logistic_regression.py
+++ logistic_regression/logistic_regression.py
@@ -9,6 +9,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from scipy.optimize import fmin_bfgs
 
 # getting the data
 f=open('ex2data1.txt','r')
@@ -49,12 +50,12 @@ m,n=X.shape
 
 def hyp_fn(theta,X):
     x=X@theta
-    return (1/(1+np.exp(-x)))
+    return (1/(1+np.exp(-x))) # sigmoid
 def cost_fn(theta,X,y):
     h=hyp_fn(theta,X)
     eps=np.finfo(float).eps # tdeal with log(0)
     #print(eps)
-    return ((-1/m)*np.sum(y.T@np.log(h+eps)+(1-y).T@(np.log(1-h+eps))))
+    return ((-0.001/m)*np.sum(y.T@np.log(h+eps)+(1-y).T@(np.log(1-h+eps))))
 def grad_fn(theta,X,y):
     h=hyp_fn(theta,X)
     return((0.001/m)*(X.T@(h-y)))
@@ -63,11 +64,39 @@ theta=np.zeros((n,1))
 print('Initial Cost: ',cost_fn(theta,X,y))
 array=[]
 input('Press enter to continue..')
-iterations=500000
+iterations=50
 for i in range(iterations):
     array.append(cost_fn(theta,X,y))
     print("Cost/Error: ",cost_fn(theta,X,y))
     theta-=grad_fn(theta,X,y)
 print("Theta: ",theta)
+plt.title('Accuracy for 50 iterations')
+plt.xlabel('Number of iterations')
+plt.ylabel('Accuracy')
 plt.plot([i for i in range(1,iterations+1)],array)
 plt.show()
+def grad_func(theta):
+    return grad_fn(theta,X,y)
+def cost_func(theta):
+    return cost_fn(theta,X,y)
+input('\nPress enter to continue..')
+theta=fmin_bfgs(cost_fn,x0=np.zeros((n,1)),args=(X,y))
+x_values=[np.min(X[:,1]),np.max(X[:,2])]
+y_values=-(theta[0]+np.dot(theta[1],x_values))/theta[2]
+plt.figure(figsize=(10,7))
+plt.title('Scatter plot of training plot')
+plt.xlabel('Marks in the first exam')
+plt.ylabel('Marks in the second exam')
+plt.scatter(plt_array_1.T[0],plt_array_1.T[1],marker='+',s=85,label='Admitted') # plotting the data
+plt.scatter(plt_array_2.T[0],plt_array_2.T[1],marker='o',c='yellow',edgecolors='black',s=85,label='Not Admitted')
+plt.plot(x_values,y_values,label='Decision Boundary')
+plt.legend(loc='upper right')
+plt.show()
+input('\nPress enter to continue..')
+predictions=[]
+for i in hyp_fn(theta,X): # Creating 1 and 0 values according to the decision boundary
+    if i>=0.5:
+        predictions.append(1)
+    else:
+        predictions.append(0)
+print('Accuracy: ',np.mean(predictions==y.T[0])*100)
